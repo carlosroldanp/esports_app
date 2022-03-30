75dd461cf5a712749e64fe262b597861686ca2a5
Carlos <carlosroldanp@gmail.com>
2022-03-30 19:08:36 +0200

    Primer commit

---
+++ src/main/java/com/croldan/esports_app/EsportsAppApplication.java
@@ -0,0 +1,13 @@
+package com.croldan.esports_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsportsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/croldan/esports_app/EsportsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.croldan.esports_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
